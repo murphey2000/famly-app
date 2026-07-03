a266e6a61203cd6bbd14366b8bab9fbd8bff32a8
newly.dev <api@newly.dev>
2026-07-03 12:46:12 +0000

    chore: Update Specular dev URL

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,7 +4,7 @@ import * as SecureStore from "expo-secure-store";
 import { Platform } from "react-native";
 import Constants from "expo-constants";
 
-const API_URL = "https://6xj9a58ah2tb4yevxebjmthvxnxv6tuh.app.specular.dev";
+const API_URL = "https://vqprhrdr6pemce78dksqkeqpdfka3x56.app.specular.dev";
 
 export const BEARER_TOKEN_KEY = "famly_bearer_token";
 
