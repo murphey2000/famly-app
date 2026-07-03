2fcac79876f3db4cbad91632127b13062222f4e7
newly.dev <api@newly.dev>
2026-07-03 12:54:38 +0000

    Fetch and merge backend URL

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,7 +4,7 @@ import * as SecureStore from "expo-secure-store";
 import { Platform } from "react-native";
 import Constants from "expo-constants";
 
-const API_URL = "https://vqprhrdr6pemce78dksqkeqpdfka3x56.app.specular.dev";
+const API_URL = "https://2gpjz4y4vbbyzy5b237skxwkqwsqua78.app.specular.dev";
 
 export const BEARER_TOKEN_KEY = "famly_bearer_token";
 
